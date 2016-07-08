a2556f99bdd74400466020e705e4f316a5ad5d2d
Sheqi <sz2632@columbia.edu>
2016-07-08 18:28:28 -0400

    reverse.c

--- reverse.c
+++ reverse.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-void recursive(char* begin){
+int main(){
+	char string[20];
+	printf("Please enter a string within 20 characters:\n");
+	fgets(string,sizeof(string),stdin);
+	char* begin;
 	char* end;
 	char a;
+	begin=string;
+	end= begin + strlen(string)-1;
 	
-	end=begin+strlen(begin)-1;
-
 	while(begin<end){
 		a=*end;
 		*end=*begin;
 		*begin=a;
-		*begin++,*end--;
+		begin++;
+		end--;
 	}
-}
 
-int main(){
-	char string;
-	printf("Please enter a string within 20 characters:\n");
-	fgets(string,20,stdin);
-	recursive(string);
 	printf("%s",string);
 	return 0;
 }
