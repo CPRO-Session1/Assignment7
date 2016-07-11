aa7b4e5606cd2720b66af70ac52f0365f71323a4
portemilio <andrew.zihenni@gmail.com>
2016-07-11 10:54:49 -0400

    Add files via upload

---
+++ return.c
@@ -0,0 +1,18 @@
+/* Did code with Justin, had some troubles with getting it to work/run */
+#Include <stdio.h>
+#Include <string.h>
+Void swap () {
+	Char string[50];
+	Printf (“Enter your string”);
+	Fgets (string, 50, stdin(;
+	Char (stringPtr = string;
+	Char *reserveString = &string(strlen(string)[];
+	
+}
+int main () {
+	while (*stringPtr != *reverseString()) {
+	printf (“%c”, *(--reverseString\n)); 
+	return 0;
+	}	
+}
+
